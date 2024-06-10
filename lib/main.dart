9551f2f6e9adbd4da1acef4762cd28b030e2090f
Maggy-Morris <78025421+Maggy-Morris@users.noreply.github.com>
2024-06-10 12:52:33 +0300

    #test CodeMagic

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
 
+// check code Magic
 void main()
 // => bootstrap();
 {
