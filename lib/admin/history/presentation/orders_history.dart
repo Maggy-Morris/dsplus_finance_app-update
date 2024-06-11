c251ee74650649b5421d23512c8abbc9083e46c4
mo7meddgom3a <mohamedgomaa0115@gmail.com>
2024-06-11 19:21:57 +0300

    fixed history filter

--- lib/admin/history/presentation/orders_history.dart
+++ lib/admin/history/presentation/orders_history.dart
@@ -139,6 +139,35 @@ class OrderHistoryBody extends StatelessWidget {
       ],
     );
   }
+  Widget buildBudgetCard(Map<String, dynamic> userData,
+      Map<String, dynamic> budgetData, DateTime startDate, DateTime endDate) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      margin: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildListTile("User Name", userData['name'] ?? ''),
+          buildListTile("Budget Name", budgetData['name'] ?? ''),
+          buildListTile("Amount", budgetData['amount']?.toString() ?? ''),
+          buildListTile("Budget Type", budgetData['type'] ?? ''),
+          buildListTile("Status", budgetData['status'] ?? ''),
+          buildListTile(
+              "Start Date", DateFormat('dd/MM/yyyy').format(startDate)),
+          endDate == startDate
+              ? SizedBox()
+              : buildListTile(
+              "End Date", DateFormat('dd/MM/yyyy').format(endDate)),
+          if (budgetData['status'] ==
+              'Rejected') // Show reason for rejected orders
+            buildListTile("Reason for Rejection", budgetData['reason'] ?? ''),
+          SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
 
   ListTile buildListTile(String title, String subtitle) {
     return ListTile(
